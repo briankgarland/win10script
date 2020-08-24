d0b65cb200db329e4c74d4fb73d16fc9c4389c9f
bgarland <briankgarland@outlook.com>
2020-08-24 13:39:37 -0600

    Added more

--- dev-setup.ps1
+++ dev-setup.ps1
@@ -9,9 +9,23 @@ choco install vscode
 
 choco install nodejs-lts
 choco install git
+choco install gulp-cli
+choco install azure-cli
+choco install docker-desktop -y
 
 #choco install sql-server-express
 #choco install sql-server-management-studio
 
+choco install bitdefenderavfree
 choco install microsoft-teams
 #choco install postman
+
+MD C:\Projects
+CD C:\Projects
+git clone https://simplot-digital.visualstudio.com/Sitecore_Projects/_git/simplot-sitecore c:\projects\SimplotSc
+CD C:/Projects/SimplotSc
+NPM install
+gulp publish-ui
+
+npm config set `@sxa:registry https://sitecore.myget.org/F/sc-npm-packages/npm/
+npm i -g `@sxa/CLI
